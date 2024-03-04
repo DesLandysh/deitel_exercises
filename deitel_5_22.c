3bbf1df8c6b3f766e105d0e229dba96115791047
Des Kitten <60898828+DesLandysh@users.noreply.github.com>
2024-03-05 01:21:07 +0300

    work but not as needed...

--- deitel_5_22.c
+++ deitel_5_22.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <limits.h>
+#include <assert.h>
+#include <math.h>
 
 /* 5.22.a */
 int res_div(int a, int b)
@@ -16,30 +18,36 @@ int res_mod(int a, int b)
 }
 
 /* 5.22.c */
-void analize_digit(short number)
+void analize_digit(int number)
 {
-  if (number < 1 || number > SHRT_MAX){
-    //temp = -1;
-    //while (temp != 0){
-    
-    printf("%d", 4);
-    printf("  ");
-    }
-  }
-  else assert("Number is not in 1..32767 range.");
+  if (number > 1 && number < SHRT_MAX){
 
+    while (number % 10 != 0){
+      printf("%d  ", number % 10);
+      number /= 10;
+    }
+    
+  }
+  else {assert("Number is not in 1..32767 range.");}
+
+  printf("\n");
 }
 
 
 int main(void)
 {
+  
   short test_number = 4562;
-  // int test_neg = -32767;     /* negative */
-  // int test_zero = 0;         /* zero */
-  // int test_int = 45621;      /* more than short int */
-  // int test_neg_int = -45621; /* more neg than short int */
+  int test_neg = -32767;     /* negative */
+  int test_zero = 0;         /* zero */
+  int test_int = 45621;      /* more than short int */
+  int test_neg_int = -45621; /* more neg than short int */
 
-  analize_digit(test_number);
+  analize_digit(test_number);   /* result */
+  analize_digit(test_neg);      /* no result */
+  analize_digit(test_zero);     /* no result */
+  analize_digit(test_int);      /* no result */
+  analize_digit(test_neg_int);  /* no result */
 
   return 0;
 }
